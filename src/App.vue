25e3f6b3de090858862b33513025d45ec66ce93e
建林 <469893894@qq.com>
2019-04-23 09:47:50 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+	import global from "./utils/global";
+
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style lang="scss">
+	@import "./scss/base";
+	@import "./scss/animate";
+	@import "./scss/variable";
+	@import "./scss/main";
+</style>
